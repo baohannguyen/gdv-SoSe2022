595df45e7bbbeb3ee1ccf5c3f73a595006abe66c
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2022-04-22 11:13:27 +0200

    Update GDV_object_counting.py

--- GDV_object_counting.py
+++ GDV_object_counting.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
+import glob
 
 
-def ChangeColor():
+def change_Color():
+    # function that adjust the color mask
     # damit wird nur die maske erstellt
     lower_color = np.array([hue - hue_range, saturation -
                             saturation_range, value - value_range])
@@ -17,27 +19,28 @@ def ChangeColor():
 
 
 def load_Image(imgNumber):
+    # function that reads the image and returns it+its parameters
     # wir haben insgesamt 6 Bilder, deswegen darf es nicht größer als 6 sein
-    if imgNumber > 6:
-        imgNumber = 6
+    if imgNumber > len(images):
+        imgNumber = 1
     if imgNumber < 1:
         # darf nicht kleiner als 1 sein, weil sonst kein Bild angezeigt wird
-        imgNumber = 1
-    imgpath = 'images\\chewing_gum_balls0'+str(imgNumber)+'.JPG'
+        imgNumber = len(images)
+    print(imgNumber)
     # da wird der bildname mit der jeweiligen nummer eingespeichert
-    img = cv2.imread(imgpath, cv2.IMREAD_COLOR)
+    img = images[imgNumber-1]
     # If set, always convert image to the 3 channel BGR  color image
     # cv2.IMREAD_COLOR
     # img = cv2.resize(img, (800, 600))
     height = img.shape[0]
     width = img.shape[1]
     last_dim = img.shape[-1]
-    return (img, height, width, last_dim)
+    return (img, height, width, last_dim, imgNumber)
     # Da werden die Bilder durchgegangen
 
 
-class Farbe:
-    def __init__(self, Name, Hue, Hue_range, Saturation, Saturation_Range, Value, Value_Range, Filter):
+class Color:
+    def __init__(self, Name, Hue, Hue_range, Saturation, Saturation_Range, Value, Value_Range, Shape, Roundness, Filter):
         self.Name = Name
         self.Hue = Hue
         self.Hue_Range = Hue_range
@@ -45,35 +48,49 @@ class Farbe:
         self.Saturation_Range = Saturation_Range
         self.Value = Value
         self.Value_Range = Value_Range
+        self.Shape = Shape
+        self.Roundness = Roundness
         self.Filter = Filter
     # die verschiedenen werte der farben
     # in eine klasse damit es ordentlicher aussieht
 
 
-farbenArray = [
-    Farbe('Blue', 100, 10, 150, 100, 255, 100, ['']),
-    Farbe('Yellow', 27, 10, 155, 100, 255, 100, ['erosion', 'erosion']),
-    Farbe('Red', 180, 10, 255, 94, 228, 101, ['dilatation', 'closing']),
-    Farbe('Pink', 0, 10, 47, 100, 255, 40, ['erosion', 'dilatation', 'closing', 'erosion']),
-    Farbe('White', 27, 10, 47, 100, 255, 20, ['opening']),
-    Farbe('Green', 43, 10, 147, 100, 147, 100, ['erosion'])]
+colorArray = [
+    Color('Blue', 100, 10, 160, 100, 255, 100, 2, 0.5, ['erosion']),
+    Color('Yellow', 27, 10, 155, 100, 255, 100, 0, 0.2, ['erosion', 'erosion', 'erosion', 'dilation']),
+    Color('Red', 180, 10, 255, 94, 228, 101, 2, 0.5, ['dilatation', 'closing']),
+    Color('Pink', 0, 10, 47, 100, 255, 40, 2, 0.5, ['erosion', 'dilatation', 'closing', 'erosion']),
+    Color('White', 27, 10, 47, 100, 255, 20, 2, 0.5, ['opening']),
+    Color('Green', 43, 10, 147, 100, 147, 100, 2, 0.5, ['erosion']),
+    Color('Red-Alt', 165, 10, 205, 100, 155, 100, 2, 0.5, ['']),
+    Color('Pink-Alt', 170, 10, 30, 40, 255, 40, 2, 0.5, ['']),
+    Color('White-alt', 0, 1, 0, 1, 255, 3, 0, 0.5, ['dilation', 'dilation', 'erosion', 'erosion', 'erosion', 'erosion', 'erosion', 'dilation', 'dilation',
+         'dilation'])]
 # die verschiedenen werte und filter der farben
 
+# get starting values
+collorNumber = 0
 
-hue = 100
-hue_range = 10
-saturation = 150
-saturation_range = 100
-value = 255
-value_range = 100
-# startwerte
+hue = colorArray[collorNumber].Hue
+hue_range = colorArray[collorNumber].Hue_Range
+saturation = colorArray[collorNumber].Saturation
+saturation_range = colorArray[collorNumber].Saturation_Range
+value = colorArray[collorNumber].Value
+value_range = colorArray[collorNumber].Value_Range
+filterList = colorArray[collorNumber].Filter
+shape = colorArray[collorNumber].Shape
+expected_roundness = colorArray[collorNumber].Roundness
+#startwerte
+
+# get all images
+images = [cv2.imread(file, cv2.IMREAD_COLOR) for file in glob.glob('images\\chewing_gum_balls**.jpg')]
 
 imgNumber = 1
-# Load the Image
-(img, height, width, last_dim) = load_Image(imgNumber)
+# Load the first Image
+(img, height, width, last_dim, imgNumber) = load_Image(imgNumber)
 
-# create a mask
-mask = ChangeColor()
+# create the first mask
+mask = change_Color()
 
 
 def morph_shape(val):
@@ -85,8 +102,13 @@ def morph_shape(val):
         return cv2.MORPH_ELLIPSE
 # die verschiedenen Formen der morphologischen Methoden
 
+kernel_size = 3
+kernel_shape = morph_shape(shape)
+# zeigt dass wir das Quadrat für die morphologischen Methoden nehmen
+
+
 # dilation with parameters
-def dilatation(img, size, shape):
+def dilation(img, size, shape):
     element = cv2.getStructuringElement(shape, (2 * size + 1, 2 * size + 1), (size, size))
     return cv2.dilate(img, element)
     # gibt den radius an
@@ -109,31 +131,31 @@ def closing(img, size, shape):
     return cv2.morphologyEx(img, cv2.MORPH_CLOSE, element)
 
 
-kernel_size = 3
-kernel_shape = morph_shape(0)
-# zeigt dass wir das Quadrat für die morphologischen Methoden nehmen
+# kernel_size = 3
+# kernel_shape = morph_shape(0)
 
 
-masknames = ['original', 'opening', 'closing', 'dilatation', 'erosion'] 
+
+
+# masknames = ['original', 'opening', 'closing', 'dilatation', 'erosion'] 
 # verschiedene Maskennamen (Methoden)
-maskNamesList = []
+# maskNamesList = []
 
 red_BGR = (0, 0, 255)
 green_BGR = (0, 255, 0)
 blue_BGR = (255, 0, 0)
 # Farben für den kreis und viereck erstellt
 # Kreis und Viereck werden genutzt um die bälle zu finden
-circle_size = 10
-circle_thickness = 5
+circle_size = 6
+circle_thickness = 3
 # Kreisgröße und dicke
 
 min_size = 10
-expected_roundness = 0.5
+# expected_roundness = 0.5
 
 found = True
 # dann kommt der satz das ein oder mehrere bälle gefunden wurde
 
-colNumber = 0
 
 while True:
     # sachen die wir nach oben übergeben
@@ -141,21 +163,23 @@ while True:
     imgcopy = img.copy()
     newmask = mask.copy()
     # maske und bild wird gekopiert
-
-    for i in range(len(maskNamesList)):
-        if maskNamesList[i] == 'opening':
+    # goes through the filterlist and morphs the mask
+    for i in range(len(filterList)):
+        if filterList[i] == 'opening':
             newmask = opening(newmask, kernel_size, kernel_shape)
             # wenn es opening ist wird in der konsole opening ausgedruckt
-        if maskNamesList[i] == 'dilatation':
-            newmask = dilatation(newmask, kernel_size, kernel_shape)
-        if maskNamesList[i] == 'closing':
+        if filterList[i] == 'dilation':
+            newmask = dilation(newmask, kernel_size, kernel_shape)
+        if filterList[i] == 'closing':
             newmask = closing(newmask, kernel_size, kernel_shape)
-        if maskNamesList[i] == 'erosion':
+        if filterList[i] == 'erosion':
             newmask = erosion(newmask, kernel_size, kernel_shape)
 
+    # gets all connected components
     connectivity = 8
     (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(newmask, connectivity, cv2.CV_32S)
     # centroids = punkte in der mitte, numlabels = anzahl der bälle (z.b bei gelb = 30)
+    # goes through all (reasonable) found connected components
     numRejected = 1
     for i in range(1, numLabels):
         # check size and roundness as plausibility
@@ -203,7 +227,7 @@ while True:
         # rechteck wird gefunden und gezeichnet
 
     if found:
-        print('We have found ' + str(numLabels-numRejected) + ' '+farbenArray[colNumber].Name + ' balls.')
+        print('We have found ' + str(numLabels-numRejected) + ' ' + colorArray[collorNumber].Name + ' balls.')
         found = False
 # Satz wird gedruckt wenn alle bedingungen erfüllt sind
     cv2.imshow('Original image', imgcopy)
@@ -211,38 +235,42 @@ while True:
 
     key = cv2.waitKey(100)
     # 100 millisekunden
+    # programm wird beendet
     if key == ord('q'):
         break
 
     if key == ord('t'):
         imgNumber = imgNumber+1
-        (img, height, width, last_dim) = load_Image(imgNumber)
-        mask = ChangeColor()
+        (img, height, width, last_dim, imgNumber) = load_Image(imgNumber)
+        mask = change_Color()
         found = True
         # damit kann man das bild ändern
 
     if key == ord('g'):
         imgNumber = imgNumber-1
-        (img, height, width, last_dim) = load_Image(imgNumber)
-        mask = ChangeColor()
+        (img, height, width, last_dim, imgNumber) = load_Image(imgNumber)
+        mask = change_Color()
         found = True
+        # damit kann man auf das vorherige bild zugreifen
 
     if key == ord('e'):
         # damit kann ich die farbe wechseln
-        colNumber = colNumber+1
-        if(colNumber > 5):
-            colNumber = 0
+        collorNumber = collorNumber+1
+        if(collorNumber > len(colorArray)-1):
+            collorNumber = 0
         found = True
 
-        hue = farbenArray[colNumber].Hue
-        hue_range = farbenArray[colNumber].Hue_Range
-        saturation = farbenArray[colNumber].Saturation
-        saturation_range = farbenArray[colNumber].Saturation_Range
-        value = farbenArray[colNumber].Value
-        value_range = farbenArray[colNumber].Value_Range
-        maskNamesList = farbenArray[colNumber].Filter
+        hue = colorArray[collorNumber].Hue
+        hue_range = colorArray[collorNumber].Hue_Range
+        saturation = colorArray[collorNumber].Saturation
+        saturation_range = colorArray[collorNumber].Saturation_Range
+        value = colorArray[collorNumber].Value
+        value_range = colorArray[collorNumber].Value_Range
+        filterList = colorArray[collorNumber].Filter
+        shape = colorArray[collorNumber].Shape
+        expected_roundness = colorArray[collorNumber].Roundness
 
-        mask = ChangeColor()
+        mask = change_Color()
         # variablen werden festgelegt
 
     if key == ord('n'):
@@ -252,7 +280,10 @@ while True:
         print('Saturation-Range : '+str(saturation_range))
         print('Value: '+str(value))
         print('Value-Range : '+str(value_range))
-        print('Filter: '+str(maskNamesList))
+        print('Shape : '+str(shape))
+        print('Roundness : '+str(expected_roundness))
+        print('Filter: '+str(filterList))
         # farbwerte werden ausgedruckt
 
 cv2.destroyAllWindows()
+# beendet das programm
